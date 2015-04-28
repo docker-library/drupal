4bc910c5e0864437f041864cc6c449966c4d3e82
Tianon Gravi <admwiggin@gmail.com>
2015-04-28 16:06:48 -0600

    Whoops, remove hard-coded version number in update.sh

--- update.sh
+++ update.sh
@@ -14,7 +14,7 @@ releases="$(curl -fsSL 'https://www.drupal.org/node/3060/release')"
 
 for version in "${versions[@]}"; do
 	fullVersion="$(echo "$ftp" | awk -F '[<> ="]+' '$2 == "a" && $3 == "href" && $4 ~ /^drupal-'"$version"'\..*\.tar\.gz$/ { gsub(/^drupal-|\.tar\.gz$/, "", $4); print $4 }' | grep -vE -- '-dev$' | sort -V | tail -1)"
-	md5="$(echo "$releases" | grep -A6 -m1 '>drupal-7.36.tar.gz<' | grep -A1 -m1 '"md5 hash"' | tail -1 | awk '{ print $1 }')"
+	md5="$(echo "$releases" | grep -A6 -m1 '>drupal-'"$fullVersion"'.tar.gz<' | grep -A1 -m1 '"md5 hash"' | tail -1 | awk '{ print $1 }')"
 	
 	(
 		set -x
