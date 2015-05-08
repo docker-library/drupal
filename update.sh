9a83a764c24f67b9184ff20d0279b911e1918011
Tianon Gravi <admwiggin@gmail.com>
2015-05-08 15:20:17 -0600

    Switch update.sh to just scrape the release page directly
    This is much faster, but more importantly, ftp.drupal.org's directory index has a very aggressive mirror that doesn't think anything newer than 7.35 even exists.

--- update.sh
+++ update.sh
@@ -9,11 +9,10 @@ if [ ${#versions[@]} -eq 0 ]; then
 fi
 versions=( "${versions[@]%/}" )
 
-curl -fSL 'http://ftp.drupal.org/files/projects/' -o ftp
 curl -fSL 'https://www.drupal.org/node/3060/release' -o release
 
 for version in "${versions[@]}"; do
-	fullVersion="$(awk -F '[<> ="]+' '$2 == "a" && $3 == "href" && $4 ~ /^drupal-'"$version"'\..*\.tar\.gz$/ { gsub(/^drupal-|\.tar\.gz$/, "", $4); print $4 }' ftp | grep -vE -- '-dev$' | sort -V | tail -1)"
+	fullVersion="$(grep -E '>drupal-'"$version"'\.[0-9a-z.-]+\.tar\.gz<' release | sed -r 's!.*<a[^>]+>drupal-([^<]+)\.tar\.gz</a>.*!\1!' | sort -V | tail -1)"
 	md5="$(grep -A6 -m1 '>drupal-'"$fullVersion"'.tar.gz<' release | grep -A1 -m1 '"md5 hash"' | tail -1 | awk '{ print $1 }')"
 	
 	(
@@ -25,4 +24,4 @@ for version in "${versions[@]}"; do
 	)
 done
 
-rm -f ftp release
+rm -f release
