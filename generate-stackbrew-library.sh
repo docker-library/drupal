44f7abcf1c2f77165c02818708d3bd722c51a602
Tianon Gravi <admwiggin@gmail.com>
2016-01-21 15:03:56 -0800

    Add FPM variants (styled after OwnCloud's script updates for this same thing)

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -15,18 +15,27 @@ url='git://github.com/docker-library/drupal'
 echo '# maintainer: InfoSiftr <github@infosiftr.com> (@infosiftr)'
 
 for version in "${versions[@]}"; do
-	commit="$(cd "$version" && git log -1 --format='format:%H' -- Dockerfile $(awk 'toupper($1) == "COPY" { for (i = 2; i < NF; i++) { print $i } }' Dockerfile))"
-	fullVersion="$(grep -m1 'ENV DRUPAL_VERSION' "$version/Dockerfile" | cut -d' ' -f3)"
-	
-	versionAliases=()
-	while [ "$fullVersion" != "$version" -a "${fullVersion%[.-]*}" != "$fullVersion" ]; do
-		versionAliases+=( $fullVersion )
-		fullVersion="${fullVersion%[.-]*}"
-	done
-	versionAliases+=( $version ${aliases[$version]} )
-	
-	echo
-	for va in "${versionAliases[@]}"; do
-		echo "$va: ${url}@${commit} $version"
+	for variant in apache fpm; do
+		commit="$(cd "$version/$variant" && git log -1 --format='format:%H' -- Dockerfile $(awk 'toupper($1) == "COPY" { for (i = 2; i < NF; i++) { print $i } }' Dockerfile))"
+		fullVersion="$(grep -m1 'ENV DRUPAL_VERSION ' "$version/$variant/Dockerfile" | cut -d' ' -f3)"
+
+		versionAliases=()
+		while [ "$fullVersion" != "$version" -a "${fullVersion%[.-]*}" != "$fullVersion" ]; do
+			versionAliases+=( $fullVersion )
+			fullVersion="${fullVersion%[.-]*}"
+		done
+		versionAliases+=( $version ${aliases[$version]} )
+
+		echo
+		for va in "${versionAliases[@]}"; do
+			if [ "$va" = 'latest' ]; then
+				echo "$variant: ${url}@${commit} $version/$variant"
+			else
+				echo "$va-$variant: ${url}@${commit} $version/$variant"
+			fi
+			if [ "$variant" = 'apache' ]; then
+				echo "$va: ${url}@${commit} $version/$variant"
+			fi
+		done
 	done
 done
