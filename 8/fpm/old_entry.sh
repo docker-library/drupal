b987a3fdaf035cd6704c222320e665e36a7049b0
Andrew Huffman <andrew.j.huffman@gmail.com>
2016-03-07 10:37:35 -0500

    Updated fpm-dockerfile and entrypoint script, updated to 8.0.5ver and md5.

--- 8/fpm/old_entry.sh
+++ 8/fpm/old_entry.sh
@@ -2,36 +2,19 @@
 
 set -e
 
-#Check for drupal version file
-if ! [ -e /var/www/html/drupal_version.txt ]; then
-  #Doesn't exist - initialize one
-  touch /var/www/html/drupal_version.txt
-else
-  DRUPAL_INSTALL=`cat /var/www/html/install_version.txt`
-  echo "Drupal-$DRUPAL_INSTALL currently installed"
-fi
-
-#Check for drupal install and version
-if ! [ -e /var/www/html/sites/default/default.settings.php ] || [ "$DRUPAL_VERSION" != "$DRUPAL_INSTALL" ]; then
-  if [ "$DRUPAL_VERSION" != "$DRUPAL_INSTALL" ]; then
-    echo >&2 "Upgrading Drupal to $DRUPAL_VERSION - copying now..."
-  else
-    echo >&2 "Drupal not found in /var/www/html/ - copying now..."
-  fi
+#Check for drupal
+if ! [ -e /var/www/html/sites/default/default.settings.php ]; then
+  echo >&2 "Drupal not found in /var/www/html/ - copying now..."
   echo >&2 "  Verifying md5..."
   echo "${DRUPAL_MD5} /usr/src/drupal-${DRUPAL_VERSION}.tar.gz" | md5sum -c - \
   && tar -xvzf /usr/src/drupal-${DRUPAL_VERSION}.tar.gz -C /var/www/html/ --strip-components=1 \
-  && chown -R www-data:www-data /var/www/html/sites \
-  && chown www-data:www-data /var/www/html \
-  && echo "${DRUPAL_VERSION}" > /var/www/html/install_version.txt
+  && chown -R www-data:www-data /var/www/html
 fi
 
 #Check for settings.php
 if ! [ -e /var/www/html/sites/default/settings.php ]; then
-  echo '/var/www/html/sites/default/settings.php not found'
-  echo '  Attempting to build settings.php from environmental variables provided'
-  
   #Start collecting parameters to build settings.php
+
   #Check DB Type
   if [ -z "$DRUPAL_DB_TYPE" ]; then
     DRUPAL_DB_TYPE='mysql'
@@ -79,40 +62,17 @@ if ! [ -e /var/www/html/sites/default/settings.php ]; then
 
   #Check Postgres Params
   if [ "$DRUPAL_DB_TYPE" = 'postgres' ]; then
-      #Check Postgres port
-    if [ -z "$PG_DB_PORT" ]; then
-      PG_DB_PORT=5432
-      echo 'No Postgres database port was provided'
-      echo '  Setting default Postgres database port to 5432'
-    fi
+    echo Postgres
+    #Check Postgres port
 
     #Check Postgres database name
-    if [ -z "$PG_DB_NAME" ]; then
-      PG_DB_NAME="postgres"
-      echo 'No Postgres database name was provided'
-      echo "  Setting default Postgres database name to 'postgres'"
-    fi
 
     #Check Postgres database user
-    if [ -z "$PG_DB_USER" ]; then
-      PG_DB_USER="postgres"
-      echo 'No Postgres database user was provided'
-      echo "  Setting default Postgres database user to 'postgres'"
-    fi
 
     #Check Postgres database host
-    if [ -z "$PG_DB_HOST" ]; then
-      echo >&2 'error: No Postgres database host was provided'
-      echo >&2 '  Provide an environment variable for PG_DB_HOST to connect to your database'
-      exit 1
-    fi
 
     #Check Postgres Password
-    if [ -z "$PG_DB_PASS" ]; then
-      echo >&2 'error: No Postgres database password was provided'
-      echo >&2 '  Provide an environment variable for PG_DB_PASS to connect to your database'
-      exit 1
-    fi
+
   fi
 
   #Check SQLite Params
@@ -145,13 +105,7 @@ if ! [ -e /var/www/html/sites/default/settings.php ]; then
       echo "  'driver' => 'mysql'," >> "$SETTINGS"
       ;;
     "postgres")
-      echo "  'database' => '"$PG_DB_NAME"'," >> "$SETTINGS"
-      echo "  'username' => '"$PG_DB_USER"'," >> "$SETTINGS"
-      echo "  'password' => '"$PG_DB_PASS"'," >> "$SETTINGS"
-      echo "  'prefix' => '"$DRUPAL_TBL_PREFIX"'," >> "$SETTINGS"
-      echo "  'host' => '"$PG_DB_HOST"'," >> "$SETTINGS"
-      echo "  'namespace' => 'Drupal\\\\Core\\\\Database\\\\Driver\\\\pgsql'," >> "$SETTINGS"
-      echo "  'driver' => 'pgsql'," >> "$SETTINGS"
+      echo Postgres
       ;;
     "sqlite")
       echo SQLite
@@ -164,21 +118,6 @@ if ! [ -e /var/www/html/sites/default/settings.php ]; then
 
   #Finish writing settings.php
   echo ");" >> "$SETTINGS"
-  HASH_SALT=$(/usr/local/bin/php << EOF
-    <?php
-      date_default_timezone_set('UTC');
-      \$date = str_replace(' ','',date("D M d, Y G:i"));
-      \$salt = hash('sha512',"\$date");
-      echo \$salt; ?>
-EOF
-)
-  echo "\$settings['hash_salt'] = '$(echo $HASH_SALT)';" >> "$SETTINGS"
-
 fi
-
-#Check if tables exist, and if not install drupal schema
-  #Need some php here to complete install
-
-echo "Attempting: $@"
 
 exec "$@"
