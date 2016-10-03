6016a31376e8b5f51abef8a02223bb94281c1e6e
Michael Stenta <mike@mstenta.net>
2016-10-02 21:31:34 -0400

    Update Drupal core if the current version does not match the desired version.

--- 7/fpm/docker-entrypoint.sh
+++ 7/fpm/docker-entrypoint.sh
@@ -4,13 +4,19 @@ set -e
 # Get the current Drupal version, if available.
 CURRENT_VERSION="$(drush --root=/var/www/html --format=yaml status | grep 'drupal-version' | sed 's/drupal-version: .\(.*\)./\1/')"
 
-# Unpack Drupal codebase, if it isn't already.
-if ! [ -e /var/www/html/sites/default/default.settings.php ]; then
-  echo >&2 "Unpacking Drupal $DRUPAL_VERSION..."
-  echo >&2 "  Verifying md5..."
-  echo "${DRUPAL_MD5} /usr/src/drupal-${DRUPAL_VERSION}.tar.gz" | md5sum -c - \
-  && tar -xvzf /usr/src/drupal-${DRUPAL_VERSION}.tar.gz -C /var/www/html/ --strip-components=1 \
-  && chown -R www-data:www-data /var/www/html/sites
+# If the  current version does not match the desired version, overwrite
+# /var/www/html with the desired Drupal codebase.
+echo >&2 "Current Drupal version: $CURRENT_VERSION"
+echo >&2 "Desired Drupal version: $DRUPAL_VERSION"
+if [ "$CURRENT_VERSION" != "$DRUPAL_VERSION" ]; then
+
+	# Unpack Drupal codebase.
+	echo >&2 "Unpacking Drupal $DRUPAL_VERSION..."
+	echo >&2 "  Verifying md5..."
+	echo "${DRUPAL_MD5} /usr/src/drupal-${DRUPAL_VERSION}.tar.gz" | md5sum -c - \
+	&& rm -rf /var/www/html/* \
+	&& tar -xvzf /usr/src/drupal-${DRUPAL_VERSION}.tar.gz -C /var/www/html/ --strip-components=1 \
+	&& chown -R www-data:www-data /var/www/html/sites
 fi
 
 # Execute the arguments passed into this script.
