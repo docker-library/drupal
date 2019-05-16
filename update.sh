343c73422b8e04c3779334f4ee9ffe48b6f84822
Tianon Gravi <admwiggin@gmail.com>
2019-05-16 16:33:51 -0700

    Switch from MD5 to SHA256

--- update.sh
+++ update.sh
@@ -42,7 +42,13 @@ for version in "${versions[@]}"; do
 	md5="${fullVersion##* }"
 	fullVersion="${fullVersion% $md5}"
 
-	echo "$version: $fullVersion ($md5)"
+	sha256="$(
+		wget -qO- "https://www.drupal.org/project/drupal/releases/$fullVersion" \
+			| tac|tac \
+			| grep -m1 'SHA-256:' | grep -oE '[0-9a-f]{64}'
+	)"
+
+	echo "$version: $fullVersion ($sha256)"
 
 	for variant in fpm-alpine fpm apache; do
 		dist='debian'
@@ -54,7 +60,7 @@ for version in "${versions[@]}"; do
 			-e 's/%%PHP_VERSION%%/'"${phpVersions[$version]:-$defaultPhpVersion}"'/' \
 			-e 's/%%VARIANT%%/'"$variant"'/' \
 			-e 's/%%VERSION%%/'"$fullVersion"'/' \
-			-e 's/%%MD5%%/'"$md5"'/' \
+			-e 's/%%SHA256%%/'"$sha256"'/' \
 		"./Dockerfile-$dist.template" > "$version/$variant/Dockerfile"
 
 		travisEnv='\n  - VERSION='"$version"' VARIANT='"$variant$travisEnv"
