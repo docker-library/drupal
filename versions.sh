9df694cb38d4ed1e62cd3b5738f7e48bdf589ca8
Joseph Ferguson <yosifkit@gmail.com>
2023-12-04 13:05:10 -0800

    Add 10.2-rc

--- versions.sh
+++ versions.sh
@@ -89,8 +89,11 @@ for version in "${versions[@]}"; do
 					version: env.fullVersion,
 					phpVersions: (
 						[
+							# https://www.drupal.org/project/drupal/releases/10.2.0-rc1#php-deps
+							# Drupal now supports PHP 8.3 and recommends at least PHP 8.2.
+							if [ "7", "10.0", "10.1" ] | index(env.version) then empty else "8.3" end,
 							"8.2",
-							"8.1",
+							if [ "7", "10.0", "10.1" ] | index(env.version) then "8.1" else empty end,
 							# https://www.drupal.org/docs/system-requirements/php-requirements
 							# https://www.drupal.org/docs/7/system-requirements/php-requirements
 							empty
