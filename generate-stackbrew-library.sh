4c58ce542e5d005de8a138ebd1ab9a47e06e6fcd
Tianon Gravi <admwiggin@gmail.com>
2024-08-07 10:23:26 -0700

    Remove PHP 8.2 from 11+

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -11,8 +11,11 @@ declare -A debianSuites=(
 	#[10.0]='bullseye'
 )
 
-defaultPhpVersion='php8.2'
+defaultPhpVersion='php8.3'
 declare -A defaultPhpVersions=(
+# releases older than 11 will conservatively stay on 8.2 by default
+	[10.3]='php8.2'
+	[10.2]='php8.2'
 # https://www.drupal.org/docs/7/system-requirements/php-requirements#php_required
 	[7]='php8.1' # PHP 7.4 & 8.0 are EOL, so we don't have a choice but to update the default
 # https://www.drupal.org/docs/system-requirements/php-requirements
--- versions.sh
+++ versions.sh
@@ -116,9 +116,16 @@ for version in "${versions[@]}"; do
 				[
 					# https://www.drupal.org/project/drupal/releases/10.2.0-rc1#php-deps
 					# Drupal now supports PHP 8.3 and recommends at least PHP 8.2.
-					if [ "7", "10.0" ] | index(env.version) then empty else "8.3" end,
-					"8.2",
-					if [ "7", "10.0" ] | index(env.version) then "8.1" else empty end,
+					if [ "7", "10.0" ] | index(env.version) then empty else
+						"8.3"
+					end,
+					# https://www.drupal.org/node/3413288 ("Drupal 11 will require PHP 8.3")
+					if [ "7", "10.0", "10.2", "10.3" ] | index(env.version) then
+						"8.2"
+					else empty end,
+					if [ "7", "10.0" ] | index(env.version) then
+						"8.1"
+					else empty end,
 					# https://www.drupal.org/docs/system-requirements/php-requirements
 					# https://www.drupal.org/docs/7/system-requirements/php-requirements
 					empty
