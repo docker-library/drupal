1eb8aca578c3dd9814712498a3cb70847cef2de0
Jesper Noordsij <jesper.noordsij@gmail.com>
2026-01-08 09:45:46 +0100

    Restore Drupal 10.6 + PHP 8.3 variant
    Also clear some old PHP version code remnant in versions.sh
    
    Co-authored-by: Joseph Ferguson <yosifkit@gmail.com>

--- versions.sh
+++ versions.sh
@@ -117,14 +117,13 @@ for version in "${versions[@]}"; do
 					end,
 					# Drupal 11.1+ and 10.4+ support PHP 8.4
 					"8.4",
-					# Drupal 11.3+ and 10.6+ recommend PHP 8.4
+					# Drupal 11.3+ and 10.6+ recommend PHP 8.4; keep 8.3 for 'existing' builds
+					# https://github.com/docker-library/drupal/pull/299
 					# https://www.drupal.org/project/drupal/releases/10.6.0#platform
 					# https://www.drupal.org/project/drupal/releases/11.3.0#platform
-					if env.version | IN("10.5", "11.2") then
+					if env.version | IN("10.5", "10.6", "11.2") then
 						"8.3"
-					else empty end,
-					# https://www.drupal.org/docs/system-requirements/php-requirements
-					empty
+					else empty end
 				]
 			),
 			variants: [
